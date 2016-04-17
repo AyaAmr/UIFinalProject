905e53e2f9104dc970b46dcd32183b2a85f89768
Aya Amr <ayoya@Ayas-MacBook-Pro-5.local>
2016-04-17 04:37:32 +0200

    Add dynamic qoutes and logos link with jquery

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@ $(document).ready(function(){
   $('.home-slider').slick({
     dots: true
   });
+
+  $('.clients-qoutes div:not(:first-child)').hide(); //Hide All qoutes except the first one
+
+  $('.clients-logos div:not(:first-child) > .active').hide(); //Hide the second logo version except for 1st element
+
+  $('.clients-logos div:first-child > .normal').hide(); //Hide the normal logo version for the first slide
+
+  /* On client logo click show client's qoute */
+  $( ".clients-logos div" ).click(function() {
+    var childNum = $(this).index() + 1;
+    $('.clients-logos div > .active').hide(); //Hide all other active logos ( normalize )
+    $('.clients-logos div > .normal').show(); //Show All original logos ( normalize )
+    $('.clients-logos div:nth-child('+childNum+') > .normal').hide(); //Hide original logo of the clicked element
+    $('.clients-logos div:nth-child('+childNum+') > .active').show(); //Show the current clicked as active
+    $('.clients-qoutes div').hide();
+    $('.clients-qoutes div:nth-child('+childNum+')').show();
+  });
+
 });
 
 
